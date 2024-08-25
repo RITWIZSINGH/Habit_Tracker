15596f0c4bbfd5177791a51115f063428d6eca12
Ritwiz <madangopalsingh2803@gmail.com>
2024-08-25 14:15:29 +0530

    Removed nav widget from main.dart to a new file

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 // ignore_for_file: must_be_immutable, use_key_in_widget_constructors, use_super_parameters
 
 import 'package:flutter/material.dart';
-import 'package:habit_tracker_app/pages/homePage.dart';
-import 'package:habit_tracker_app/pages/habitsPage.dart';
-import 'package:habit_tracker_app/pages/profilePage.dart';
-import 'package:habit_tracker_app/pages/progressPage.dart';
+import 'package:habit_tracker_app/screens/nav_screen.dart';
 
 
 void main() {
@@ -24,50 +21,6 @@ class MyApp extends StatelessWidget {
       ),
       home: NavigationScreen(
         currentIndex: 3,
-      ),
-    );
-  }
-}
-
-class NavigationScreen extends StatefulWidget {
-  NavigationScreen({required this.currentIndex});
-  int currentIndex;
-  @override
-  State<NavigationScreen> createState() => _NavigationScreenState();
-}
-
-const List<Widget> screens = [
-  HomePage(),
-  ProgressPage(),
-  HabitsPage(),
-  ProfilePage()
-];
-
-class _NavigationScreenState extends State<NavigationScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: widget.currentIndex,
-        children: screens,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        backgroundColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-        selectedItemColor: Colors.deepPurpleAccent,
-        currentIndex: widget.currentIndex,
-        onTap: (index) {
-          setState(() {
-            widget.currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_today), label: "Progress"),
-          BottomNavigationBarItem(icon: Icon(Icons.timeline), label: "Habits"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
-        ],
       ),
     );
   }
---
+++ lib/screens/nav_screen.dart
@@ -0,0 +1,53 @@
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+import 'package:habit_tracker_app/pages/homePage.dart';
+import 'package:habit_tracker_app/pages/habitsPage.dart';
+import 'package:habit_tracker_app/pages/profilePage.dart';
+import 'package:habit_tracker_app/pages/progressPage.dart';
+
+
+class NavigationScreen extends StatefulWidget {
+  NavigationScreen({required this.currentIndex});
+  int currentIndex;
+  @override
+  State<NavigationScreen> createState() => _NavigationScreenState();
+}
+
+const List<Widget> screens = [
+  HomePage(),
+  ProgressPage(),
+  HabitsPage(),
+  ProfilePage()
+];
+
+class _NavigationScreenState extends State<NavigationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: widget.currentIndex,
+        children: screens,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Colors.deepPurpleAccent,
+        currentIndex: widget.currentIndex,
+        onTap: (index) {
+          setState(() {
+            widget.currentIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.calendar_today), label: "Progress"),
+          BottomNavigationBarItem(icon: Icon(Icons.timeline), label: "Habits"),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile")
+        ],
+      ),
+    );
+  }
+}
