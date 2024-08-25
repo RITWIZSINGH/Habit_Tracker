eb4b6f108f4cef057b32d907a938cb79911a29c0
Ritwiz <madangopalsingh2803@gmail.com>
2024-08-25 14:47:12 +0530

    Added some photos to the assets

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,9 +1,10 @@
-// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, use_super_parameters, file_names
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, use_super_parameters, file_names, prefer_const_constructors, sort_child_properties_last, avoid_print, curly_braces_in_flow_control_structures
 
 import 'package:flutter/material.dart';
+import 'package:habit_tracker_app/services/lists.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({super.key});
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
