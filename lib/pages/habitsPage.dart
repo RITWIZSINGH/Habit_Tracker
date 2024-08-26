46d50db1484e7c098dcdad540faacb70757f9752
Ritwiz <madangopalsingh2803@gmail.com>
2024-08-26 22:42:28 +0530

    create a chartsBuilder service for the habits page

--- lib/pages/habitsPage.dart
+++ lib/pages/habitsPage.dart
@@ -1,9 +1,9 @@
-// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, use_super_parameters, file_names
+// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, use_super_parameters, file_names, prefer_const_constructors, sized_box_for_whitespace
 
 import 'package:flutter/material.dart';
-
+import 'package:habit_tracker_app/services/chartsBuilder.dart';
 class HabitsPage extends StatefulWidget {
-  const HabitsPage({super.key});
+  const HabitsPage({Key? key}) : super(key: key);
 
   @override
   State<HabitsPage> createState() => _HabitsPageState();
@@ -12,6 +12,6 @@ class HabitsPage extends StatefulWidget {
 class _HabitsPageState extends State<HabitsPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Placeholder();
   }
 }
