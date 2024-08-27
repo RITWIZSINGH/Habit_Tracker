602d3f78c2941bec1acec71cabf49bed110c282e
Ritwiz <madangopalsingh2803@gmail.com>
2024-08-27 12:51:38 +0530

    created a profile page structure

--- lib/pages/profilePage.dart
+++ lib/pages/profilePage.dart
@@ -1,9 +1,9 @@
-// ignore_for_file: must_be_immutable, use_key_in_widget_constructors, use_super_parameters, file_names
+// ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
-  const ProfilePage({super.key});
+  const ProfilePage({Key? key}) : super(key: key);
 
   @override
   State<ProfilePage> createState() => _ProfilePageState();
@@ -12,6 +12,76 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: ListView(
+        padding: EdgeInsets.fromLTRB(15, 50, 15, 20),
+        children: [
+          CircleAvatar(
+            radius: 150,
+            backgroundColor: Colors.purpleAccent,
+            backgroundImage: NetworkImage(
+                'https://cdn2.iconfinder.com/data/icons/avatars-60/5985/12-Delivery_Man-512.png'),
+          ),
+          // ignore: prefer_const_constructors
+          Padding(
+            padding: EdgeInsets.only(top: 25, bottom: 10),
+            child: const Center(
+                child: Text(
+              "Hey Hermano!",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold),
+            )),
+          ),
+          Center(
+              child: Text(
+            "What a wonderful day!!",
+            style: TextStyle(
+                color: Colors.black, fontSize: 16, fontWeight: FontWeight.w400),
+          )),
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: Divider(),
+          ),
+          ListTile(
+            leading: Icon(Icons.person),
+            title: Text("My Account Info"),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.payment),
+            title: Text("My Subscription Info"),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.list),
+            title: Text("All of my habits"),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          Divider(),
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text("About This App"),
+            trailing: Icon(Icons.arrow_forward_ios),
+          ),
+          Divider(),
+          Center(
+            child: TextButton(
+              onPressed: (() {
+                print("User logged out");
+              }),
+              child: Text(
+                "Log Out",
+                style:
+                    TextStyle(color: Colors.red, fontWeight: FontWeight.bold),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
