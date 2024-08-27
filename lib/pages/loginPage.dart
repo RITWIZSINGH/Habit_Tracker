12520139c05e02a315d81394e6ac5d608f7aebc3
Ritwiz <madangopalsingh2803@gmail.com>
2024-08-28 00:43:32 +0530

    Minor change

--- lib/pages/loginPage.dart
+++ lib/pages/loginPage.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_super_parameters, file_names, avoid_print, use_build_context_synchronously, library_private_types_in_public_api, use_key_in_widget_constructors, sort_child_properties_last
+// ignore_for_file: prefer_const_constructors, use_super_parameters, file_names, avoid_print, use_build_context_synchronously, library_private_types_in_public_api, use_key_in_widget_constructors, sort_child_properties_last, unused_import
 
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
